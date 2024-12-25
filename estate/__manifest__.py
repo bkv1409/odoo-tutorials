4d4c9b17e5101159879b4a92c8a2918cb4b6a0dc
Nguyen Quang Vu <q.vunguyen@okxe.vn>
2024-12-25 11:37:33 +0700

    add estate step 4 to git

---
+++ estate/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+    'summary': """
+        Starting module for "Real Estate"
+    """,
+
+    'description': """
+        Starting module for "Real Estate"
+    """,
+
+    'version': '0.1',
+    'application': True,
+    'category': 'Tutorials/Estate',
+    'installable': True,
+    'depends': ['base', 'web'],
+    'data': [
+        "security/ir.model.access.csv",
+    ],
+    'assets': {
+        'web.assets_backend': [
+
+        ],
+    },
+    'license': 'AGPL-3'
+}
---
+++ estate/__manifest__bk.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Starting module for "Estate"
+    """,
+
+    'description': """
+        Starting module for "Estate"
+    """,
+
+    'author': "VuNQ",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials/Estate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+
+    'data': [],
+    'license': 'AGPL-3'
+}
