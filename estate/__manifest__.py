03b1bc861a5ef64bdf8cf9613e2d5b4428b800d8
Nguyen Quang Vu <q.vunguyen@okxe.vn>
2024-12-25 13:20:14 +0700

    update file full for module estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -16,6 +16,12 @@
     'depends': ['base', 'web'],
     'data': [
         "security/ir.model.access.csv",
+        "views/estate_menus.xml",
+        "views/estate_property_views.xml",
+        "views/estate_property_offer_views.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_property_type_views.xml",
+        "views/res_users_views.xml",
     ],
     'assets': {
         'web.assets_backend': [
